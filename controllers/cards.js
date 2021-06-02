5fa30d5f0cfb9588cd355d381b67557a2df9b0ff
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2021-06-02 23:37:16 +0300

    id

--- controllers/cards.js
+++ controllers/cards.js
@@ -24,11 +24,21 @@ module.exports.createCard = (req, res) => {
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.id)
-  .orFail(() => {
-    return res.status(404).send({message:"Карточка с указанным _id не найдена."})
-  })
+  .orFail(
+    () => {
+      throw new Error()
+    }
+  )
   .then((card) => res.status(200).send({ data: card }))
-  .catch(err => res.status(500).send({ message: 'Ошибка' }));
+  .catch((err) =>{
+    if(err.kind === "ObjectId") {
+      const code = 404;
+      const error = `Карточка с указанным _id не найдена!`;
+      res.status(code).send({ messages: error })
+    } else {
+     res.status(500).send({ message: 'Ошибка' })
+    }
+  })
 };
 
 module.exports.likeCard = (req, res) => {
@@ -37,11 +47,21 @@ module.exports.likeCard = (req, res) => {
   { $addToSet: { likes: req.user._id } },
   { new: true }
 )
-.orFail(() => {
-  return res.status(404).send({message:"Карточка с указанным _id не найдена."})
-})
+.orFail(
+  () => {
+    throw new Error()
+  }
+)
 .then((card) => res.status(200).send({ data: card }))
-.catch(err => res.status(500).send({ message: 'Ошибка' }));
+.catch((err) =>{
+  if(err.kind === "ObjectId") {
+    const code = 404;
+    const error = `Карточка с указанным _id не найдена`;
+    res.status(code).send({ messages: error })
+  } else {
+   res.status(500).send({ message: 'Ошибка' })
+  }
+})
 };
 
 module.exports.dislikeCard = (req, res) => {
@@ -50,9 +70,19 @@ module.exports.dislikeCard = (req, res) => {
   { $pull: { likes: req.user._id } },
   { new: true }
 )
-.orFail(() => {
-  return res.status(404).send({message:"Карточка с указанным _id не найдена."})
-})
+.orFail(
+  () => {
+    throw new Error()
+  }
+)
 .then((card) => res.status(200).send({ data: card }))
-.catch(err => res.status(500).send({ message: 'Ошибка' }));
+.catch((err) =>{
+  if(err.kind === "ObjectId") {
+    const code = 404;
+    const error = `Карточка с указанным _id не найдена`;
+    res.status(code).send({ messages: error })
+  } else {
+   res.status(500).send({ message: 'Ошибка' })
+  }
+})
 };
